6341ac2b320047bb4e096a7c862c81fece909160
SanketKadam1330 <133142831+SanketKadam1330@users.noreply.github.com>
2023-05-10 21:53:23 +0530

    Create Student.java

---
+++ Student.java
@@ -0,0 +1,39 @@
+package com.dto;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+@Entity
+public class Student
+{
+	@Id
+int id ;
+String name ;
+String gender ;
+int age ;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+}
